ad93bfb1809e33ea6e48d4388b1dec20c904b9e0
Freshia <freshiasackey@gmail.com>
2019-09-25 10:31:23 +0300

    fixed data type error

--- test_regression.py
+++ test_regression.py
@@ -1,4 +1,7 @@
 import LinearRegression as ln
+import numpy as np
 def test_square_error():
-    square_error = ln.squared_error(5,8)
+    origs = [5,6,7]
+    line = [6,7,10]
+    square_error = ln.squared_error(np.array(origs, dtype=np.float64),np.array(line, dtype=np.float64))
     assert square_error!=0
